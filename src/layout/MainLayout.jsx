bb0d618ab568d5399494e44012c503f83df7b02d
mehedi hasan <mhshohan04@gmail.com>
2025-06-16 16:01:26 +0600

    register page added

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from '../pages/shared/Navbar'
 
 const MainLayout = () => {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
   )
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout';
 import Home from '../pages/Home/Home';
+import Register from '../pages/register/Register';
 
   const router = createBrowserRouter([
     {
@@ -10,7 +11,11 @@ import Home from '../pages/Home/Home';
         {
           path: '/',
           element: <Home></Home>
-        }
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        },
       ]
     },
   ]);
