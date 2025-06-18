d59868759b220c8fa819176927dd92d2546818ef
mehedi hasan <mhshohan04@gmail.com>
2025-06-18 15:29:01 +0600

    signin page with authentication and lottie animation

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import MainLayout from '../layout/MainLayout';
 import Home from '../pages/Home/Home';
 import Register from '../pages/register/Register';
+import SignIn from '../pages/signin/SignIn';
 
   const router = createBrowserRouter([
     {
@@ -15,6 +16,10 @@ import Register from '../pages/register/Register';
         {
           path: '/register',
           element: <Register></Register>
+        },
+        {
+          path: '/signin',
+          element: <SignIn></SignIn>
         },
       ]
     },
