2d5dc826013f55b134b22fbd1d717b3f88b3da3a
mehedi hasan <mhshohan04@gmail.com>
2025-06-18 10:40:32 +0600

    create context

---
+++ src/context/AuthContext/AuthProvider.jsx
@@ -0,0 +1,11 @@
+import AuthContext from "./AuthContext"
+
+const AuthProvider = ({children}) => {
+  return (
+    <AuthContext.Provider>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
