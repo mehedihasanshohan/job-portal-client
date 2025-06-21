e325b3ea91672e5f2edc61fbbf3fcde9c91e0fa0
mehedi hasan <mhshohan04@gmail.com>
2025-06-21 11:40:55 +0600

    auth direct

--- src/router/PrivateRoute.jsx
+++ src/router/PrivateRoute.jsx
@@ -1,14 +1,19 @@
 import { useContext } from "react";
-import { Navigate} from "react-router-dom";
+import { Navigate, useLocation} from "react-router-dom";
 import AuthContext from "../context/AuthContext/AuthContext";
 
 const PrivateRoute = ({ children }) => {
 
-  const {user} = useContext(AuthContext);
+  const {user, loading} = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
 
+  if(loading){
+    return <span className="loading loading-ring loading-lg"></span>
+  }
 
   if (!user) {
-    return <Navigate to="/signin" />;
+    return <Navigate to="/signin" state={location?.pathname}/>;
   }
 
   return children;
